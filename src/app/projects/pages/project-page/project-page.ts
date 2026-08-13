738cee68e8d3cf30170c54d7cc60f69154dcf233
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-13 15:53:44 -0400

    Se setea título para cada proyecto individualmente

--- src/app/projects/pages/project-page/project-page.ts
+++ src/app/projects/pages/project-page/project-page.ts
@@ -1,8 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 import { ProjectService } from '../../services/project-service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-project-page',
@@ -10,21 +11,16 @@ import { ProjectService } from '../../services/project-service';
   templateUrl: './project-page.html',
   styleUrl: './project-page.css',
 })
-export class ProjectPage implements OnInit {
-
+export class ProjectPage {
 
   private activatedRoute = inject(ActivatedRoute);
   private projectService = inject(ProjectService);
-
-  ngOnInit(): void {
-    this.projectService.getProjectById(1).subscribe(foo => {
-      console.log(foo);
-    });
-  }
+  private title = inject(Title);
 
   project = toSignal(
     this.activatedRoute.params.pipe(
-      switchMap(params => this.projectService.getProjectById(parseInt(params['id'])))
+      switchMap(params => this.projectService.getProjectById(parseInt(params['id']))),
+      tap(project => this.title.setTitle(`Proyecto ${project?.title}`))
     )
   );
 }
