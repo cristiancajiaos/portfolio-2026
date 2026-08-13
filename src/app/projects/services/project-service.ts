14224f0d6b915599eb1cea4cb43855d70537b826
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-13 15:36:24 -0400

    Se crea caché para la obtención de proyecto individual en el servicio ProjectService

--- src/app/projects/services/project-service.ts
+++ src/app/projects/services/project-service.ts
@@ -9,20 +9,26 @@ export class ProjectService {
   private http = inject(HttpClient);
 
   mapProjects = new Map<string, Project[]>;
+  mapProject = new Map<number, Project | undefined>;
 
   getProjects(): Observable<Project[] | undefined> {
     if (this.mapProjects.get('projects')) {
       return of(this.mapProjects.get('projects'));
     }
-    
+
     return this.http.get<Project[]>(`assets/json/projects.json`).pipe(
       tap((projects) => this.mapProjects.set('projects', projects))
     )
   }
 
   getProjectById(id: number): Observable<Project | undefined> {
+    if (this.mapProject.get(id)) {
+      return of(this.mapProject.get(id));
+    }
+    
     return this.http.get<Project[]>(`assets/json/projects.json`).pipe(
       map((projects) => projects.find(project => project.id == id)),
+      tap((project) => this.mapProject.set(id, project))
     );
   }
 }
