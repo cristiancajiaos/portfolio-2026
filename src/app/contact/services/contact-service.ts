1ce9c744bb0425797149bff11012fc3ca9db06b2
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-13 15:43:57 -0400

    Se crea caché para la obtención de contactos en el servicio ContactService

--- src/app/contact/services/contact-service.ts
+++ src/app/contact/services/contact-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { ContactMedium } from '../interfaces/contact-medium';
 
 @Service()
@@ -8,7 +8,15 @@ export class ContactService {
 
   private http = inject(HttpClient);
 
-  getContacts(): Observable<ContactMedium[]> {
-    return this.http.get<ContactMedium[]>(`assets/json/contacts.json`);
+  private mapContacts = new Map<string, ContactMedium[] | undefined>;
+
+  getContacts(): Observable<ContactMedium[] | undefined> {
+    if (this.mapContacts.has('contacts')) {
+      return of(this.mapContacts.get('contacts'));
+    }
+    
+    return this.http.get<ContactMedium[]>(`assets/json/contacts.json`).pipe(
+      tap(contactMediums => this.mapContacts.set('contacts', contactMediums))
+    )
   }
 }
