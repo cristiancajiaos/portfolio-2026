9b95a3aaec901974d0c64e413325c99d6c88968d
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-13 15:32:47 -0400

    Se crea caché para la obtención de proyectos en el servicio ProjectService

--- src/app/projects/services/project-service.ts
+++ src/app/projects/services/project-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 import { Project } from '../interfaces/project-interface';
 
 @Service()
@@ -8,8 +8,16 @@ export class ProjectService {
 
   private http = inject(HttpClient);
 
-  getProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(`assets/json/projects.json`);
+  mapProjects = new Map<string, Project[]>;
+
+  getProjects(): Observable<Project[] | undefined> {
+    if (this.mapProjects.get('projects')) {
+      return of(this.mapProjects.get('projects'));
+    }
+    
+    return this.http.get<Project[]>(`assets/json/projects.json`).pipe(
+      tap((projects) => this.mapProjects.set('projects', projects))
+    )
   }
 
   getProjectById(id: number): Observable<Project | undefined> {
